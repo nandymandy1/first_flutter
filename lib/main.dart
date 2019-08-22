0db25288c3e60dd493ae9e2473313c6560b6207a
Narendra Maurya <narendra.maurya@in.accemy.com>
2019-08-23 00:57:11 +0530

    Button and layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,34 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  Widget build(BuildContext context){
+
+  List<String> animals = [
+    "lion",
+    "cat",
+    "dogs"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello World"),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(children: <Widget>[
+          Text("The Question"),
+          RaisedButton(child: Text("Answer 1"), onPressed: () => answerQuestion(1),),
+          RaisedButton(child: Text("Answer 2"), onPressed: () => answerQuestion(2),),
+          RaisedButton(child: Text("Answer 3"), onPressed: (){ print('Answer 3 chosen'); },),
+        ],),
+      ),
     );
   }
+
+  // Methods
+  void answerQuestion(int option){
+    print('Answer $option chosen');
+  }
+
 }
