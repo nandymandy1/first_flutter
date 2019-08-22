060320a2e24a5556a19a37e4ee84c24a96e0e946
Narendra Maurya <narendra.maurya@in.accemy.com>
2019-08-23 01:34:02 +0530

    State management

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
 
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+
+  var questionIndex = 0;
   List<String> questions = [
     "What is your favorit color?",
     "What is your favorite language?",
@@ -19,19 +28,27 @@ class MyApp extends StatelessWidget {
           title: Text('My First App'),
         ),
         body: Column(children: <Widget>[
-          Text(questions.elementAt(1)),
+          Text(questions.elementAt(questionIndex)),
           RaisedButton(child: Text("Answer 1"), onPressed: () => answerQuestion(1),),
           RaisedButton(child: Text("Answer 2"), onPressed: () => answerQuestion(2),),
-          RaisedButton(child: Text("Answer 3"), onPressed: (){ print('Answer 3 chosen'); },),
+          RaisedButton(child: Text("Answer 3"), onPressed: () => answerQuestion(3),),
         ],),
       ),
     );
   }
 
-  // Video Time 3:23:30
-
   // Methods
   void answerQuestion(int option){
     print('Answer $option chosen');
+    setState(() {
+      if(questionIndex == 2){
+        questionIndex = 0;
+      } else {
+        questionIndex = questionIndex + 1;
+      }
+    });
   }
+
 }
+
+// Video Time 3:41:35
